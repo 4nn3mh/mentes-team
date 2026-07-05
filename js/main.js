75ebd2c65964b9b94b3bdc4b07f8907bc60683cb
Andrea Esmeralda Machado Hernandez <andreaesmeralda056@gmail.com>
2026-07-04 23:59:20 -0700

    Actualización sin terminar 04/07/2026

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-document.getElementById("return-button").addEventListener("click", function () {
-    history.back();
-});
+const btn = document.getElementById("return-button");
+
+if (btn) {
+    btn.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        console.log("Historial:", history.length);
+        console.log("Referrer:", document.referrer);
+
+        if (document.referrer) {
+            window.location.href = document.referrer;
+        } else if (history.length > 1) {
+            history.back();
+        } else {
+            window.location.href = "index.html";
+        }
+    });
+}
