35bf30a6851022321848a3a2c3b92a6156e21132
Andrea Esmeralda Machado Hernandez <andreaesmeralda056@gmail.com>
2026-07-05 22:16:23 -0700

    Actualización sin terminar 05/07/2026

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-const btn = document.getElementById("return-button");
+document.addEventListener("DOMContentLoaded", () => {
+    //Botón regresar universal
+    const returnBtn = document.getElementById("return-button");
 
-if (btn) {
-    btn.addEventListener("click", function (e) {
-        e.preventDefault();
+    if (returnBtn) {
+        returnBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (window.history.length > 1) {
+                window.history.back();
+            } else {
+                window.location.href = "index.html";
+            }
+        });
+    }
 
-        console.log("Historial:", history.length);
-        console.log("Referrer:", document.referrer);
+    //Carrusel de imágenes de la página principal (index.html)
 
-        if (document.referrer) {
-            window.location.href = document.referrer;
-        } else if (history.length > 1) {
-            history.back();
-        } else {
-            window.location.href = "index.html";
+    const slides = document.querySelectorAll(".slide");
+    const next = document.querySelector(".next");
+    const prev = document.querySelector(".prev");
+
+    if (slides.length && next && prev) {
+        let index = 0;
+        function showSlide(i){
+            slides.forEach(slide => slide.classList.remove("active"));
+            slides[i].classList.add("active");
         }
-    });
-}
+        next.addEventListener("click", () => {
+            index = (index + 1) % slides.length;
+            showSlide(index);
+        });
+        prev.addEventListener("click", () => {
+            index = (index - 1 + slides.length) % slides.length;
+            showSlide(index);
+        });
+        setInterval(() => {
+            index = (index + 1) % slides.length;
+            showSlide(index);
+        }, 4000);
+    }
+});
